ac8afd62d12e68efe3647464f94d0fc6b87efc06
tao.qit <tao.qit@antfin.com>
2021-02-08 20:45:40 +0800

    feat: 列头名称 numberToAlpha

--- src/core/Spreadsheet.js
+++ src/core/Spreadsheet.js
@@ -1,7 +1,7 @@
 import defaultConfig from '../configs/defaultConfig'
 import defaultSheetData from '../configs/defaultSheetData'
 import defaultTheme from '../configs/defaultTheme'
-import { assignStyle, perf } from '../utils/utils'
+import { assignStyle, perf, numberToAlpha } from '../utils/utils'
 import '../style/core.scss'
 
 class Spreadsheet {
@@ -29,42 +29,16 @@ class Spreadsheet {
     this.sheetData = defaultSheetData(this.opts, this.theme)
 
     perf(() => {
-      this.drawHeader()
-    }, 'drawHeader')
+      this.drawHeaderPerf()
+    }, 'drawHeaderPerf')
+
+    // perf(() => {
+    //   this.drawHeader()
+    // }, 'drawHeader')
 
     perf(() => {
       this.drawBody()
     }, 'drawBody')
-  }
-
-  drawHeader() {
-    const { ctx, sheetData, theme } = this
-    const { colMeta, rowMeta } = sheetData
-
-    ctx.beginPath()
-    assignStyle(ctx, theme.header)
-
-    for (let i = 0; i < colMeta.length; i += 1) {
-      const col = colMeta[i]
-      this.drawHeaderCell(
-        col,
-        { offset: 0, size: theme.colHeaderHeight },
-        i + 1
-      )
-    }
-
-    for (let j = 0; j < rowMeta.length; j += 1) {
-      const row = rowMeta[j]
-      this.drawHeaderCell({ offset: 0, size: theme.rowHeaderWidth }, row, j + 1)
-    }
-
-    this.drawHeaderCell(
-      { offset: 0, size: theme.rowHeaderWidth },
-      { offset: 0, size: theme.colHeaderHeight },
-      ''
-    )
-
-    ctx.save()
   }
 
   drawBody() {
@@ -93,6 +67,130 @@ class Spreadsheet {
     ctx.lineTo(col.offset, row.offset + row.size)
   }
 
+  drawHeaderPerf() {
+    const { ctx, theme } = this
+
+    ctx.beginPath()
+    assignStyle(ctx, theme.header)
+
+    this.drawHeaderFillStyle()
+    this.drawHeaderFillText()
+    this.drawHeaderStroke()
+
+    this.drawHeaderCell(
+      { offset: 0, size: theme.rowHeaderWidth },
+      { offset: 0, size: theme.colHeaderHeight },
+      ''
+    )
+
+    ctx.save()
+  }
+
+  drawHeaderFillStyle() {
+    const { ctx, sheetData, theme } = this
+    const { colMeta, rowMeta } = sheetData
+    const { fillStyle } = theme.header
+
+    ctx.fillStyle = fillStyle
+
+    for (let i = 0; i < colMeta.length; i += 1) {
+      const col = colMeta[i]
+      const row = { offset: 0, size: theme.colHeaderHeight }
+      ctx.rect(col.offset, row.offset, col.size, row.size)
+    }
+
+    for (let j = 0; j < rowMeta.length; j += 1) {
+      const row = rowMeta[j]
+      const col = { offset: 0, size: theme.rowHeaderWidth }
+      ctx.rect(col.offset, row.offset, col.size, row.size)
+    }
+
+    ctx.fill()
+  }
+
+  drawHeaderStroke() {
+    const { ctx, sheetData, theme } = this
+    const { colMeta, rowMeta } = sheetData
+    const { strokeStyle } = theme.header
+
+    this.ctx.beginPath()
+    ctx.strokeStyle = strokeStyle
+
+    for (let i = 0; i < colMeta.length; i += 1) {
+      const col = colMeta[i]
+      const row = { offset: 0, size: theme.colHeaderHeight }
+      ctx.moveTo(col.offset, row.offset)
+      ctx.lineTo(col.offset + col.size, row.offset)
+      ctx.moveTo(col.offset, row.offset)
+      ctx.lineTo(col.offset, row.offset + row.size)
+    }
+
+    for (let j = 0; j < rowMeta.length; j += 1) {
+      const row = rowMeta[j]
+      const col = { offset: 0, size: theme.rowHeaderWidth }
+      ctx.moveTo(col.offset, row.offset)
+      ctx.lineTo(col.offset + col.size, row.offset)
+      ctx.moveTo(col.offset, row.offset)
+      ctx.lineTo(col.offset, row.offset + row.size)
+    }
+
+    ctx.stroke()
+  }
+
+  drawHeaderFillText() {
+    const { ctx, sheetData, theme } = this
+    const { colMeta, rowMeta } = sheetData
+    const { color } = theme.header
+
+    ctx.fillStyle = color
+    // 列头
+    for (let i = 0; i < colMeta.length; i += 1) {
+      const col = colMeta[i]
+      const row = { offset: 0, size: theme.colHeaderHeight }
+      ctx.fillText(
+        numberToAlpha(i),
+        col.offset + col.size / 2,
+        row.offset + row.size / 2
+      )
+    }
+    // 行头
+    for (let j = 0; j < rowMeta.length; j += 1) {
+      const row = rowMeta[j]
+      const col = { offset: 0, size: theme.rowHeaderWidth }
+      ctx.fillText(j + 1, col.offset + col.size / 2, row.offset + row.size / 2)
+    }
+  }
+
+  drawHeader() {
+    const { ctx, sheetData, theme } = this
+    const { colMeta, rowMeta } = sheetData
+
+    ctx.beginPath()
+    assignStyle(ctx, theme.header)
+
+    for (let i = 0; i < colMeta.length; i += 1) {
+      const col = colMeta[i]
+      this.drawHeaderCell(
+        col,
+        { offset: 0, size: theme.colHeaderHeight },
+        numberToAlpha(i)
+      )
+    }
+
+    for (let j = 0; j < rowMeta.length; j += 1) {
+      const row = rowMeta[j]
+      this.drawHeaderCell({ offset: 0, size: theme.rowHeaderWidth }, row, j + 1)
+    }
+
+    this.drawHeaderCell(
+      { offset: 0, size: theme.rowHeaderWidth },
+      { offset: 0, size: theme.colHeaderHeight },
+      ''
+    )
+
+    ctx.save()
+  }
+
   drawHeaderCell(col, row, text) {
     const { ctx, theme } = this
     const { fillStyle, strokeStyle, color } = theme.header
@@ -112,11 +210,7 @@ class Spreadsheet {
 
     // 文字
     ctx.fillStyle = color
-    ctx.fillText(
-      text + '',
-      col.offset + col.size / 2,
-      row.offset + row.size / 2
-    )
+    ctx.fillText(text, col.offset + col.size / 2, row.offset + row.size / 2)
   }
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,6 +4,18 @@ export function assignStyle(ctx, styles) {
   })
 }
 
+// TODO: 非递归调用，性能有些小问题
+const COLUMN_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const COLUMN_CHARSET_LENGTH = COLUMN_CHARSET.length
+export function numberToAlpha(number) {
+  const mode = COLUMN_CHARSET.substr(number % COLUMN_CHARSET_LENGTH, 1)
+  const up = Math.floor(number / COLUMN_CHARSET_LENGTH)
+  if (up > 0) {
+    return `${numberToAlpha(up - 1)}${mode}`
+  }
+  return mode
+}
+
 export function perf(func, name) {
   const t0 = window.performance.now()
   func.apply(this)
