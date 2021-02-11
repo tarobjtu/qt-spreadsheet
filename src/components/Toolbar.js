1abcab88bead0e5862a63fa38ddf23a74fbd084a
tao.qit <tao.qit@antfin.com>
2021-02-12 07:31:32 +0800

    feat(toolbar): 顶部工具条

---
+++ src/components/Toolbar.js
@@ -0,0 +1,16 @@
+import configs from '../configs/toolbar'
+import './toolbar.scss'
+
+class Toolbar {
+  constructor({ root }) {
+    this.root = root
+
+    this.initElements()
+  }
+
+  initElements() {
+    const { root } = this
+  }
+}
+
+export default Toolbar
---
+++ src/configs/toolbar.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    key: 'undo',
+    name: '撤销',
+    icon: 'assets/icons/undo.png',
+  },
+  {
+    key: 'redo',
+    name: '重做',
+    icon: 'assets/icons/redo.png',
+  },
+  {
+    key: 'divider',
+  },
+  {
+    key: 'bold',
+    name: '粗体',
+    icon: 'assets/icons/undo.png',
+  },
+  {
+    key: 'italic',
+    name: '斜体',
+    icon: 'assets/icons/italic.png',
+  },
+  {
+    key: 'underline',
+    name: '下划线',
+    icon: 'assets/icons/underline.png',
+  },
+  {
+    key: 'font-color',
+    name: '字体颜色',
+    icon: 'assets/icons/font-color.png',
+  },
+  {
+    key: 'background-color',
+    name: '背景颜色',
+    icon: 'assets/icons/background-color.png',
+  },
+  {
+    key: 'divider',
+  },
+  {
+    key: 'merge-cell',
+    name: '合并单元格',
+    icon: 'assets/icons/merge-cell.png',
+  },
+]
