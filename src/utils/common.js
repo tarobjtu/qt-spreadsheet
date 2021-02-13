4e2dc1a759750f60af0f2a7f66b4c9681b895ac1
tao.qit <tao.qit@antfin.com>
2021-02-13 17:14:15 +0800

    feat: 深拷贝函数

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,6 +4,14 @@ export function assignStyle(ctx, styles) {
   })
 }
 
+/**
+ * @description 深拷贝，比lodash的cloneDeep性能高3倍，不知为啥...
+ * @param {*} object
+ */
+export function deepClone(object) {
+  return JSON.parse(JSON.stringify(object))
+}
+
 // TODO: 非递归调用，性能有些小问题
 const COLUMN_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const COLUMN_CHARSET_LENGTH = COLUMN_CHARSET.length
@@ -22,6 +30,11 @@ export function perf(func, name) {
   func.apply(this)
   const t1 = window.performance.now()
   console.warn(name, t1 - t0)
+}
+
+// 在开发环境快速查看电子表格核心数据，调试便利
+if (process.env.NODE_ENV === 'development') {
+  window.perf = perf
 }
 
 export default {
